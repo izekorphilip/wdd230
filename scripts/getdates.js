a966529f803a49f0e88a11650aa75bcee2e59a45
izekorphilip <izekorphilip5@gmail.com>
2024-02-02 19:19:50 +0100

    week 3 assignment updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,11 @@ let year = d.getFullYear();
 document.getElementById('year').innerHTML =  (`Ⓒ ${year}`);
 
 document.getElementById('lastModified').innerHTML = (`Last Modifification: ${document.lastModified}`);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
