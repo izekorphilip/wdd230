7ffadccc3ad75961a92019f84af9b0bddf56fb82
izekorphilip <izekorphilip5@gmail.com>
2024-02-26 22:43:41 +0100

    newly updated

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,3 +9,21 @@ hamButton.addEventListener('click', () => {
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById('lastModified').innerHTML = (`Last Modifification: ${document.lastModified}`);
+
+
+const checkButton = document.querySelector('.input');
+const webMain = document.querySelector('main');
+const WebBody = document.querySelector('body');
+const section = document.querySelector('.card-11');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+
+checkButton.addEventListener('click', () => {
+	webMain.classList.toggle('dark-mode');
+	WebBody.classList.toggle('dark-mode');
+	section.classList.toggle('dark-mode');
+	header.classList.toggle('dark-mode');
+	footer.classList.toggle('dark-mode');
+});
+
