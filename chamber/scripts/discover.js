e99f24a71de9becdee73c62122ac69a64630e1ac
izekorphilip <izekorphilip5@gmail.com>
2024-03-17 19:17:31 +0100

    newly committed

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,13 +1,71 @@
 
+
+
+const day = new Date();
+let year = day.getFullYear();
+
+
+document.getElementById('year').innerHTML = (`Ⓒ ${year}`);
+
+document.getElementById('lastModified').innerHTML = (`Last Modifification: ${document.lastModified}`);
+
+/*
 let visited = document.querySelector('.visited');
 let numbers = document.querySelector('.num');
-/*
+
 let newDate = Date.now();
 localStorage.setItem('date', newDate);
-let realTime = localStorage.getItem('date');*/
+let realTime = localStorage.getItem('date');
 
 
 let newDate = localStorage.setItem( 'current', Date.now());
 
-let firstvisit = localStorage.getItem('current') || 0;
-if(firstvisit ) 
+let firstvisit = Number(window.localStorage.getItem('current'))|| 0;
+if(firstvisit !== 0){
+    visited.textContent = firstvisit;
+}else{
+    visited.textContent = 'You are welcome here friend!'
+}
+firstvisit++;
+localStorage.setItem('current', firstvisit);
+
+*/
+/*
+let newDate = localStorage.setItem( 'current', new Date());
+
+// 1️⃣ Initialize display element variable
+const visitsDisplay = document.querySelector(".visited");
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(localStorage.getItem("current")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+	visitsDisplay.textContent =  numVisits;
+}
+else {
+	visitsDisplay.textContent = 'Welcome! Let us know if you have any questions.';
+}
+// 4️⃣ increment the number of visits by one.
+
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
+localStorage.clear();
+
+// 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
+
+*/
+
+let newDate = localStorage.setItem( 'current', Date.now());
+
+const visitsDisplay = document.querySelector(".visited");
+
+let firstvisit = window.localStorage.getItem('visitedOne');
+if (firstvisit == null){
+    visitsDisplay.textContent = 'Welcome! Let us know if you have any';   
+}else{
+    visitsDisplay.textContent = 'Back so soon! Awesome!'
+}
+localStorage.setItem('visitedOne', 1);
+localStorage
---
+++ scripts/form.js
@@ -0,0 +1,52 @@
+
+let password1 = document.querySelector('#pwd');
+let password2 = document.querySelector('#pwd2');
+let displayMes = document.querySelector('.display');
+
+password2.addEventListener('focusout', passwordPass);
+
+function passwordPass(){
+    if(password1.value !== password2.value){
+        displayMes.textContent = 'Your password do not match!.';
+        password1.value = "";
+        password1.focus();
+        password2.value = "";
+        displayMes.style.color = 'black'
+
+    }else{
+        displayMes.textContent = 'Password Matched!';
+		password2.style.backgroundColor = "#fff";
+		password2.style.color = "#000";
+    }
+
+}
+
+const button = document.querySelector(".button");
+
+function displayText() {
+    let text = document.getElementById( 'click', ".button");
+    text.style.display = "block";
+  }
+
+/*
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
+*/
